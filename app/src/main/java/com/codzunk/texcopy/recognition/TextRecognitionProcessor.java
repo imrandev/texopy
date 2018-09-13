5859eb8fd0f25f9855428124c1e85e10da40e7a6
imrandev <context.imran@gmail.com>
2018-09-14 03:42:45 +0600

    Initial Commit

---
+++ app/src/main/java/com/codzunk/texcopy/recognition/TextRecognitionProcessor.java
@@ -0,0 +1,64 @@
+package com.codzunk.texcopy.recognition;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.codzunk.texcopy.FrameMetadata;
+import com.codzunk.texcopy.GraphicOverlay;
+import com.codzunk.texcopy.VisionProcessorBase;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TextRecognitionProcessor extends VisionProcessorBase<FirebaseVisionText> {
+    private static final String TAG = "TextRecProc";
+
+    private final FirebaseVisionTextRecognizer detector;
+
+    public TextRecognitionProcessor() {
+        detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Text Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<FirebaseVisionText> detectInImage(FirebaseVisionImage image) {
+        return detector.processImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @NonNull FirebaseVisionText results,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+        graphicOverlay.clear();
+        List<FirebaseVisionText.TextBlock> blocks = results.getTextBlocks();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    GraphicOverlay.Graphic textGraphic = new TextGraphic(graphicOverlay, elements.get(k));
+                    graphicOverlay.add(textGraphic);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.w(TAG, "Text detection failed." + e);
+    }
+}
